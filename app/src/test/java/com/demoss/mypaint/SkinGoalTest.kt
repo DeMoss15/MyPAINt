7cf70b588159cde67b35a119e3ecbc28aeab64b8
Daniel <mossur15@gmail.com>
2019-04-27 10:04:47 +0300

    improved logic for skinGoal

--- app/src/test/java/com/demoss/mypaint/SkinGoalTest.kt
+++ app/src/test/java/com/demoss/mypaint/SkinGoalTest.kt
@@ -2,8 +2,13 @@ package com.demoss.mypaint
 
 import org.junit.Test
 
+const val EMPTY_STRING = ""
+
 class SkinGoalTest {
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Test data
+    ///////////////////////////////////////////////////////////////////////////
     val routineIds = listOf<Int>(1, 2, 3, 4, 5)
 
     val skinGoals = listOf<SkinGoal>(
@@ -13,8 +18,8 @@ class SkinGoalTest {
         SkinGoal(listOf(3, 4, 5))
     )
 
-    val achievementsSet = mapOf<SkinGoal, AchievementSet>(
-        skinGoals[0] to AchievementSet(
+    val achievementsSet = listOf<AchievementSet>(
+        AchievementSet(
             0, 0, listOf(
                 Achievement(4, 1),
                 Achievement(4, 2),
@@ -24,7 +29,7 @@ class SkinGoalTest {
             ), skinGoals[0]
         ),
 
-        skinGoals[1] to AchievementSet(
+        AchievementSet(
             0, 0, listOf(
                 Achievement(4, 1),
                 Achievement(4, 2),
@@ -33,7 +38,7 @@ class SkinGoalTest {
             ), skinGoals[1]
         ),
 
-        skinGoals[2] to AchievementSet(
+        AchievementSet(
             0, 0, listOf(
                 Achievement(3, 1),
                 Achievement(3, 2),
@@ -73,57 +78,83 @@ class SkinGoalTest {
         )
     )
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Main part - logic
+    ///////////////////////////////////////////////////////////////////////////
     @Test
     fun run() {
-        weeks.map { week ->
-            val skinGoalAppearancesOnWeek = mutableMapOf<SkinGoal, Int>()
-            skinGoals.forEach {
-                skinGoalAppearancesOnWeek[it] = 0
-            }
-            // how much days according to each goal
-            week.useLogs.forEach { log ->
-                skinGoals.forEach { goal ->
-                    skinGoalAppearancesOnWeek[goal]?.let {
-                        skinGoalAppearancesOnWeek[goal] = it + if (log.isLogFitsGoal(goal)) 1 else 0
-                    }
-                }
-            }
-            // check for achievement
-            skinGoalAppearancesOnWeek.forEach { (goal, appPerThisWeek) ->
-                achievementsSet[goal]?.apply {
-                    with(achievements[currentAchievement]) {
-                        if (appPerThisWeek >= trainingsPerWeek) {
-                            currentWeek++
-                            if (currentWeek == weeks) currentAchievement++
-                        } else {
-                            currentWeek = 0
-                            currentAchievement = 0
+        for (week in weeks) {
+            achievementsFor@ for (achieveSet in achievementsSet) {
+                var counterPerThisWeek = 0
+                for (log in week.useLogs) {
+                    if (log.isLogFitsGoal(achieveSet.goal)) counterPerThisWeek++
+                    if (achieveSet.firstDate == EMPTY_STRING) achieveSet.firstDate = log.date
+                    if (counterPerThisWeek == achieveSet.achievements[achieveSet.currentAchievementPosition].trainingsPerWeek) {
+                        ///////////////////////////////////////////////////////////////////////////
+                        // here we have reached achievement, week, log of day [when reached]
+                        // counterPerThisWeek may be replaced with list of logs
+                        // and use it's size like counter [in this case we will have all logs for day, dates reached from - to]
+                        ///////////////////////////////////////////////////////////////////////////
+                        achieveSet.achievements[achieveSet.currentAchievementPosition].apply {
+                            if (achieveSet.currentWeek + 1 == trainingsPerWeek) {
+                                achievedTimes++
+                                date = log.date
+                            }
                         }
+                        // current week has custom setter that increments currentAchievementPosition when needed
+                        achieveSet.currentWeek++
+                        // we can't use named rules from lambda, that's why I didn't use apply or something like that for achieveSet
+                        continue@achievementsFor
                     }
                 }
+                achieveSet.currentWeek = 0
+                achieveSet.currentAchievementPosition = 0
             }
         }
 
+        // here is our results
         achievementsSet.forEach {
-            print(it.value.currentAchievement)
+            print(it.currentAchievementPosition)
         }
     }
+}
 
-    private fun UseLog.isLogFitsGoal(goal: SkinGoal): Boolean =
-        this.routineIds.containsAll(goal.routineIds)
+private fun UseLog.isLogFitsGoal(goal: SkinGoal): Boolean =
+    this.routineIds.containsAll(goal.routineIds)
 
-    data class SkinGoal(val routineIds: List<Int>)
+data class SkinGoal(val routineIds: List<Int>)
 
-    data class UseLog(val routineIds: List<Int>)
+data class UseLog(val routineIds: List<Int>) {
+    val date: String = "some date"
+}
 
-    data class Week(val useLogs: List<UseLog>, val achievements: MutableList<Achievement>)
+data class Week(val useLogs: List<UseLog>, val achievements: MutableList<Achievement>)
 
-    data class Achievement(val trainingsPerWeek: Int, val weeks: Int)
+data class Achievement(val trainingsPerWeek: Int, val weeks: Int) {
 
-    data class AchievementSet(
-        var currentWeek: Int,
-        var currentAchievement: Int,
-        val achievements: List<Achievement>,
-        val goal: SkinGoal
-    )
+    var achievedTimes: Int = 0
+    var date: String = EMPTY_STRING
+}
+
+class AchievementSet(
+    _currentWeek: Int,
+    var currentAchievementPosition: Int,
+    val achievements: List<Achievement>,
+    val goal: SkinGoal
+) {
+
+    var firstDate: String = EMPTY_STRING
+    set(value) { if (field == EMPTY_STRING) field = value }
+
+    var currentWeek = _currentWeek
+        set(value) {
+            if (value == achievements[currentAchievementPosition].weeks) currentAchievementPosition++
+            // cycling of achievements
+            if (currentAchievementPosition == achievements.size) {
+                currentAchievementPosition = 0
+                field = 0
+            } else {
+                field = value
+            }
+        }
 }
